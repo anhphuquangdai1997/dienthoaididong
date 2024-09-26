f040ba2920729ae81adc35196b5760b69e9ccbf5
the-movie-phimmoi <daiphu094@gmail.com>
2024-09-26 16:27:04 +0700

    cap nhâp 3

--- src/view/PriceSearch.jsx
+++ src/view/PriceSearch.jsx
@@ -7,7 +7,7 @@ const PriceSearch = ({sortOrder, setSortOrder}) => {
         setShowPriceOptions(!showPriceOptions)
     }
     return (
-        <div className='flex items-baseline'>
+        <div className='flex'>
             <p className='justify-center px-2 text-[rgba(102,112,133,1)]'>Sắp xếp theo</p>
             <button
                 type="button"
@@ -25,10 +25,10 @@ const PriceSearch = ({sortOrder, setSortOrder}) => {
                 <button
                     onClick={togglePriceOptions}
                     type="button"
-                    className="text-[rgba(102,112,133,1)] bg-[rgba(242,244,247,1)] hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-3 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                    className="text-[rgba(102,112,133,1)] bg-[rgba(242,244,247,1)] hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-0 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
                 >
                     Lọc theo giá
-                    <span className={`ml-2 transition-transform ${showPriceOptions ? 'rotate-180' : ''}`}>
+                    <span className={`text-sm text-gray-600 transition-transform ${showPriceOptions ? 'rotate-180' : ''}`}>
                         ▼ {/* Mũi tên chỉ xuống */}
                     </span>
                 </button>
