4839dbbdce4e43750f08d099931708a32810a93b
anhphuquangdai1997 <47989188+anhphuquangdai1997@users.noreply.github.com>
2024-12-24 03:42:09 +0700

    Update Adtiment.jsx

--- src/view/Adtiment.jsx
+++ src/view/Adtiment.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import oneimages from "../images/4810ab203ec6286f99c11cfd7d9f5917.webp";
+import two from "../images/aaa05119320eec8a193a5fc40ca4d69d.webp";
+import three from "../images/5db8807174f55f0ae5b898429b9dad31.webp"
 
 const Adtiment = () => {
     return (
         <div className='grid-cols-1 px-2 hidden md:block'>
-            <div className='max-w-56 py-2'><img src="https://cdni.dienthoaivui.com.vn/690x300,webp,q100/https://dashboard.dienthoaivui.com.vn/uploads/wp-content/uploads/images/ee6078e59b9a448658a7720e33aa39a0.png" alt="" /></div>
-            <div className='max-w-56 py-2'><img src="https://cdni.dienthoaivui.com.vn/690x300,webp,q100/https://dashboard.dienthoaivui.com.vn/uploads/wp-content/uploads/images/ffd47cf5d35467f771cf237ef0388f61.png" alt="" /></div>
-            <div className='max-w-56 py-2'><img src="https://cdni.dienthoaivui.com.vn/690x300,webp,q100/https://dashboard.dienthoaivui.com.vn/uploads/wp-content/uploads/images/8348edb22cc4bc52cd5b05b78d3c4d38.png" alt="" /></div>
+            <div className='max-w-56 py-2'><img src={oneimages} alt="" /></div>
+            <div className='max-w-56 py-2'><img src={two} alt="" /></div>
+            <div className='max-w-56 py-2'><img src={three} alt="" /></div>
         </div>
     )
 }
